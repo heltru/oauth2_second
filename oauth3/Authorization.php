28c14170066b078f58e9f045079978d8b036c8a1
heltru <laneo2007@yandex.ru>
2021-10-02 04:50:37 +0300

    first commit

---
+++ oauth3/Authorization.php
@@ -0,0 +1,123 @@
+<?php
+
+
+class Authorization
+{
+
+    public $auth_code_lifetime = 10000;//sec
+
+    /*
+     * begin --> senler.ru/oauth2/authorize?response_type=code&client_id=1937553&state=user&scope=delivery bots leads&redirect_uri=http://app.com/callback_senler
+     *
+     * end   --> "http://app.com/callback_senler?code=1f9572946684b4b15d8b01cd427fb378def07fd8&state=user"
+     */
+
+    public function begin()
+    {
+        $is_authorized = (Service\Sessions::getAuthUser()) ? true : false; //user дб залогинен в senler
+        if ($is_authorized === false) {
+            return false;
+        }
+        $user_id = VkSender / Core / App::getUserId(); // authorization_codes привязывается к user_id
+        $state = $_REQUEST['state'];
+        $scope = $_REQUEST['scope'];
+        $client_id = $_REQUEST['client_id'];
+
+
+        $clientData = $this->db->prepare(sprintf('SELECT * from oauth_clients where client_id = :client_id'));
+        if (!$clientData) {
+            return false;
+        }
+
+        $registered_redirect_uri = $clientData['redirect_uri'];
+        if (!$this->validateRedirectUri($_REQUEST['redirect_uri'], $registered_redirect_uri)) {
+            return false;
+        };
+
+        if (!$this->checkScope($scope, $clientData['scope'])) {
+            return false;
+        };
+
+        $params = array(
+            'scope' => $scope,
+            'state' => $state,
+            'client_id' => $client_id,
+            'redirect_uri' => $registered_redirect_uri
+        );
+
+        $code = $this->generateAuthorizationCode();
+        $code_ = $this->db->prepare(sprintf('SELECT * from oauth_authorization_codes where authorization_code = :code'));
+
+        $expires = date('Y-m-d H:i:s', time() + $this->auth_code_lifetime);
+        if ($code_) {//скорей всего ни когда не найдет
+            $stmt = $this->db->prepare($sql = sprintf('UPDATE oauth_authorization_codes SET client_id=:client_id, user_id=:user_id, redirect_uri=:redirect_uri, expires=:expires, scope=:scope where authorization_code=:code'));
+        } else {
+            $stmt = $this->db->prepare(sprintf('INSERT INTO oauth_authorization_codes (authorization_code, client_id, user_id, redirect_uri, expires, scope) VALUES (:code, :client_id, :user_id, :redirect_uri, :expires, :scope)'));
+        }
+
+
+        header("Location: http://{$clientData['redirect_uri']}/?code={$code}&state={$state}");
+        exit;
+
+    }
+
+
+    ///helpers
+
+
+    public function checkScope($required_scope, $available_scope)
+    {
+        $required_scope = explode(' ', trim($required_scope));
+        $available_scope = explode(' ', trim($available_scope));
+
+        return (count(array_diff($required_scope, $available_scope)) == 0);
+    }
+
+    protected function generateAuthorizationCode()
+    {
+        $tokenLen = 40;
+        if (function_exists('random_bytes')) {
+            $randomData = random_bytes(100);
+        } elseif (function_exists('openssl_random_pseudo_bytes')) {
+            $randomData = openssl_random_pseudo_bytes(100);
+        } elseif (function_exists('mcrypt_create_iv')) {
+            $randomData = mcrypt_create_iv(100, MCRYPT_DEV_URANDOM);
+        } elseif (@file_exists('/dev/urandom')) { // Get 100 bytes of random data
+            $randomData = file_get_contents('/dev/urandom', false, null, 0, 100) . uniqid(mt_rand(), true);
+        } else {
+            $randomData = mt_rand() . mt_rand() . mt_rand() . mt_rand() . microtime(true) . uniqid(mt_rand(), true);
+        }
+
+        return substr(hash('sha512', $randomData), 0, $tokenLen);
+    }
+
+    private function validateRedirectUri($inputUri, $registeredUriString)
+    {
+        if (!$inputUri || !$registeredUriString) {
+            return false; // if either one is missing, assume INVALID
+        }
+
+        $registered_uris = preg_split('/\s+/', $registeredUriString);
+        foreach ($registered_uris as $registered_uri) {
+            if (strcmp($inputUri, $registered_uri) === 0) {
+                return true;
+
+            } else {
+                $registered_uri_length = strlen($registered_uri);
+                if ($registered_uri_length === 0) {
+                    return false;
+                }
+
+                // the input uri is validated against the registered uri using case-insensitive match of the initial string
+                // i.e. additional query parameters may be applied
+                if (strcasecmp(substr($inputUri, 0, $registered_uri_length), $registered_uri) === 0) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+}
---
+++ oauth3/Token.php
@@ -0,0 +1,118 @@
+<?php
+
+
+class Token
+{
+    public $access_lifetime = 10000;//sec
+
+/*
+ * begin --> https://senler.ru/oauth2/token&code=1f9572946684b4b15d8b01cd427fb378def07fd8
+ * end -- {"access_token":"03807cb390319329bdf6c777d4dfae9c0d3b3c35","expires_in":3600,"scope":delivery bot lead stat}
+ *
+ * */
+    //
+    public function begin(){
+
+        $clientParams = ['client_secret'=>$_REQUEST['client_secret'],'client_id'=>$_REQUEST['client_id']];
+
+        $clientData = $this->db->prepare(sprintf('SELECT * from oauth_clients where client_id = :client_id'));
+         
+        if ($clientData['client_secret'] != $clientParams['client_secret']){
+            return  false;
+        }
+
+        $code = $_REQUEST('code');
+
+        $authCode = $this->db->prepare(sprintf('SELECT * from oauth_authorization_codes where authorization_code = :code'));
+        if ($authCode) {
+            $authCode['expires'] = strtotime($code['expires']);
+        }
+        if (urldecode($_REQUEST['redirect_uri']) != urldecode($authCode['redirect_uri'])){
+            return  false;
+        }
+        if ($authCode["expires"] < time()) {
+              return false;
+        }
+        if ($authCode['clientId'] != $clientData['client_id']){
+            return false;
+        }
+
+        $requestedScope = $_REQUEST['scope'];
+        $availableScope = $clientData['scope'];
+        if (!$this->checkScope($requestedScope, $availableScope)){
+            $scope = $this->getDefaultScope($clientData['client_id']);
+        } else {
+            $scope = $availableScope;
+        }
+
+         $token = array(
+             "access_token" => $this->generateAccessToken(),
+             "expires_in" => $this->access_lifetime,
+             "scope" => $scope
+         );
+
+        $expires = date('Y-m-d H:i:s', time() + $this->access_lifetime );
+        $token = $this->db->prepare(sprintf('SELECT * from oauth_access_tokens where access_token = :access_token'));
+        if ($token) {//скорей всего ни когда не найдет
+            $token = $this->db->prepare(sprintf('UPDATE oauth_access_tokens SET client_id=:client_id, expires=:expires, user_id=:user_id, scope=:scope where access_token=:access_token'));
+        } else {
+            $token = $this->db->prepare(sprintf('INSERT INTO oauth_access_tokens (access_token, client_id, expires, user_id, scope) VALUES (:access_token, :client_id, :expires, :user_id, :scope)'));
+        }
+
+        header('Content-Type: application/json; charset=utf-8');
+        $arr = [
+            'access_token'=> $token['access_token'],
+            'expires_in' => $expires
+        ];
+
+        echo json_encode($arr);
+
+    }
+
+
+    public function checkScope($required_scope, $available_scope)
+    {
+        $required_scope = explode(' ', trim($required_scope));
+        $available_scope = explode(' ', trim($available_scope));
+
+        return (count(array_diff($required_scope, $available_scope)) == 0);
+    }
+
+    public function getDefaultScope($client_id = null)
+    {
+        return 'delivery bot lead subscription lead_stat delivery_stat global_vars process utms';
+    }
+
+    protected function generateAccessToken()
+    {
+        if (function_exists('random_bytes')) {
+            $randomData = random_bytes(20);
+            if ($randomData !== false && strlen($randomData) === 20) {
+                return bin2hex($randomData);
+            }
+        }
+        if (function_exists('openssl_random_pseudo_bytes')) {
+            $randomData = openssl_random_pseudo_bytes(20);
+            if ($randomData !== false && strlen($randomData) === 20) {
+                return bin2hex($randomData);
+            }
+        }
+        if (function_exists('mcrypt_create_iv')) {
+            $randomData = mcrypt_create_iv(20, MCRYPT_DEV_URANDOM);
+            if ($randomData !== false && strlen($randomData) === 20) {
+                return bin2hex($randomData);
+            }
+        }
+        if (@file_exists('/dev/urandom')) { // Get 100 bytes of random data
+            $randomData = file_get_contents('/dev/urandom', false, null, 0, 20);
+            if ($randomData !== false && strlen($randomData) === 20) {
+                return bin2hex($randomData);
+            }
+        }
+        // Last resort which you probably should just get rid of:
+        $randomData = mt_rand() . mt_rand() . mt_rand() . mt_rand() . microtime(true) . uniqid(mt_rand(), true);
+
+        return substr(hash('sha512', $randomData), 0, 40);
+    }
+
+}
